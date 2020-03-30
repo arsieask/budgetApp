475392864c8005b826ba696c48f2b4528bb0ca71
arsieask <54412949+arsieask@users.noreply.github.com>
2020-03-30 15:35:18 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+var budgetController = (function (){
+
+	
+
+})();
+
+
+var UIController = (function() {
+
+
+
+})();
+
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+	var ctrlAddItem = function(){console.log('hey')};
+
+
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+	 document.addEventListener('keypress', function(event){
+
+	 	if (event.keyCode === 13){
+	 		console.log('Enter was pressed')
+	 	}
+
+	 });
+
+
+
+})(budgetController, UIController);
+
+
+
