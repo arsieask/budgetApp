aca726690ac26a440e0824c2d98b39da745237b0
Arsen Askandaryan <arsieask@gmail.com>
2020-04-01 16:09:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,13 +6,24 @@ var budgetController = (function (){
 
 var UIController = (function() {
 
+	var DOMstrings = {
+		inputType: '.add__type',
+		inputDescription: '.add__description',
+		inputValue: '.add__value',
+		inputBtn: '.add__btn'
+	}
+
 	return{
 		getInput: function() {
 			return{
-				type: document.querySelector('.add__type').value,
-				description: document.querySelector('.add__description').value,
-				value: document.querySelector('.add__value').value,
-			}
+				type: document.querySelector(DOMstrings.inputType).value,
+				description: document.querySelector(DOMstrings.inputDescription).value,
+				value: document.querySelector(DOMstrings.inputValue).value,
+			};
+		},
+
+		getDOMstrings: function() {
+			return DOMstrings;
 		}
 	}
 
@@ -21,13 +32,15 @@ var UIController = (function() {
 
 var controller = (function(budgetCtrl, UICtrl) {
 
+	var DOM = UICtrl.getDOMstrings();
+
 	var ctrlAddItem = function(){
 		var input = UICtrl.getInput();
 		console.log(input);
 	};
 
 
-	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
 	 document.addEventListener('keypress', function(event) {
 
