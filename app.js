b29a5749055f96d85864556d673d8cf96baac02c
Arsen Askandaryan <arsieask@gmail.com>
2020-04-10 11:26:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ var budgetController = (function () {
 			} else {
 				data.percentage = -1;
 			}
-			
+
 		},
 
 		getBudget: function() {
@@ -104,7 +104,11 @@ var UIController = (function() {
 		inputValue: '.add__value',
 		inputBtn: '.add__btn',
 		incomeContainer: '.income__list',
-		expensesContainer: '.expenses__list'
+		expensesContainer: '.expenses__list',
+		budgetLabel: '.budget__value',
+		incomeLabel: '.budget__income--value',
+		expensesLabel: '.budget__expenses--value',
+		percentageLabel: '.budget__expenses--percentage'
 	}
 
 	return{
@@ -152,6 +156,22 @@ var UIController = (function() {
 			feildsArr[0].focus();
 		},
 
+		displayBudget: function(obj) {
+
+			document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+			document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+			document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+			if(obj.percentage > 0) {
+				document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+			} else {
+				document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+
+			}
+
+		},
+
+
 		getDOMstrings: function() {
 			return DOMstrings;
 		}
@@ -186,7 +206,7 @@ var controller = (function(budgetCtrl, UICtrl) {
  		var budget = budgetCtrl.getBudget();
 
  		//Display the budget on the UI
- 		console.log(budget);
+ 		UICtrl.displayBudget(budget);
 
 	};
 
@@ -216,6 +236,12 @@ var controller = (function(budgetCtrl, UICtrl) {
 	return{
 		init: function() {
 			console.log('init works');
+			UICtrl.displayBudget({
+				budget: 0,
+				totalInc: 0,
+				totalExp: 0,
+				percentage: -1
+			});
 			setupEventListeners();
 		}
 	};
